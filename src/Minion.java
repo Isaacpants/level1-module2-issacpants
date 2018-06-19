f2e4c6002147ec03b0541ed27117fc016abde9fa
League Student <league@iMac-6.attlocal.net>
2018-06-19 12:11:20 -0700

    new level 1 mod 2 repo

---
+++ src/Minion.java
@@ -0,0 +1,51 @@
+
+public class Minion {
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+public Minion(String string, int i, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	name=string;
+	eyes=i;
+	color=string2;
+	master=string3;
+	}
+public String getName() {
+	return name;
+	
+	
+}
+public int getEyes() {
+	return eyes;
+	
+	
+}
+public String getColor() {
+	return color;
+	
+	
+}
+public String getMaster() {
+	return master;
+	
+	
+}
+public void setEyes(int newEyes) {
+	
+	eyes=newEyes;
+	
+}
+public void setColor(String newColor) {
+	
+	color=newColor;
+	
+}
+public void setMaster(String newMaster) {
+	
+	master=newMaster;
+	
+}
+
+
+}
